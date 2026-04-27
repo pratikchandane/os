7392ed2c93065eb82506272d1cfec3b22587b001
Pratik Chandane <155938540+pratikchandane@users.noreply.github.com>
2026-04-28 01:51:55 +0530

    Create 18CSCANdisk.c

---
+++ 18CSCANdisk.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+int main() {
+    int req[20], left[20], right[20];
+    int n, i, j;
+    int head;
+    int diskSize = 200;   // assuming disk range 0–199
+    int leftCount = 0, rightCount = 0;
+    int total_movement = 0;
+
+    printf("Enter number of requests (min 15): ");
+    scanf("%d", &n);
+
+    printf("Enter disk request queue:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &req[i]);
+    }
+
+    printf("Enter initial head position: ");
+    scanf("%d", &head);
+
+    // Divide into left and right of head
+    for(i = 0; i < n; i++) {
+        if(req[i] < head) {
+            left[leftCount++] = req[i];
+        } else {
+            right[rightCount++] = req[i];
+        }
+    }
+
+    // Sort left array (descending)
+    for(i = 0; i < leftCount - 1; i++) {
+        for(j = i + 1; j < leftCount; j++) {
+            if(left[i] < left[j]) {
+                int temp = left[i];
+                left[i] = left[j];
+                left[j] = temp;
+            }
+        }
+    }
+
+    // Sort right array (ascending)
+    for(i = 0; i < rightCount - 1; i++) {
+        for(j = i + 1; j < rightCount; j++) {
+            if(right[i] > right[j]) {
+                int temp = right[i];
+                right[i] = right[j];
+                right[j] = temp;
+            }
+        }
+    }
+
+    printf("\nDisk Movement Sequence:\n");
+    printf("%d ", head);
+
+    // Move towards left (0 direction)
+    for(i = 0; i < leftCount; i++) {
+        total_movement += (head - left[i]);
+        head = left[i];
+        printf("-> %d ", head);
+    }
+
+    // Go to end of disk (0)
+    total_movement += head;
+    head = 0;
+    printf("-> %d ", head);
+
+    // Move towards right direction
+    for(i = 0; i < rightCount; i++) {
+        total_movement += (right[i] - head);
+        head = right[i];
+        printf("-> %d ", head);
+    }
+
+    printf("\n\nTotal Head Movement = %d\n", total_movement);
+
+    return 0;
+}
